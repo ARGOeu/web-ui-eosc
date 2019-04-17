b61db7306dc6d714473e97911091d9faac063dae
cyril <cyril.lorphelin@cc.in2p3.Fr>
2019-04-17 09:25:43 +0200

    MAJ

--- argo-start.sh
+++ argo-start.sh
@@ -2,7 +2,8 @@
 if [ -z "$M2_HOME" ] ; then
     export M2_HOME=/usr/share/maven
 fi
-sed /usr/bin/mvn \
-    -e 's|plexus-classworlds-\*.jar|plexus-classworlds.jar:${HOME}/.m2/repository/com/sun/mail/logging-mailhandler/1.6.2/logging-mailhandler-1.6.2.jar:${HOME}/.m2/repository/com/sun/mail/javax.mail/1.6.2/javax.mail-1.6.2.jar|' \
+cd /opt/argo-eosc
+sed /usr/share/maven/bin/mvn \
+    -e 's|plexus-classworlds.jar|plexus-classworlds.jar:${HOME}/.m2/repository/com/sun/mail/logging-mailhandler/1.6.2/logging-mailhandler-1.6.2.jar:${HOME}/.m2/repository/com/sun/mail/javax.mail/1.6.2/javax.mail-1.6.2.jar|' \
     -e 's|$@|exec:java|' \
 | sh
--- argo-stop.sh
+++ argo-stop.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-PORT=`sed ${0%/*}/etc/service/service.properties -e '/^lavoisier.http.port=/!d' -e 's/^lavoisier.http.port=//'`
+cd /opt/argo-eosc
+HTTP_DISABLE=`sed ${0%/*}/../etc/service/service.properties -e '/^lavoisier.http.disable=/!d' -e 's/^lavoisier.http.disable=//'`
+if test "${HTTP_DISABLE}" = "true" ; then
+    PORT=`sed ${0%/*}/../etc/service/service.properties -e '/^lavoisier.https.port=/!d' -e 's/^lavoisier.https.port=//'`
+else
+    PORT=`sed ${0%/*}/../etc/service/service.properties -e '/^lavoisier.http.port=/!d' -e 's/^lavoisier.http.port=//'`
+fi
 curl http://localhost:${PORT}/lavoisier/restart
