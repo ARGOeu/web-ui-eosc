84f1710291aaa76a45aeff46420fae63131a8781
Cyril L'Orphelin <cyril.lorphelin@cc.in2p3.fr>
2019-07-09 14:47:55 +0200

    Custom reports details

--- etc/app/js/dataTable.js
+++ etc/app/js/dataTable.js
@@ -4,6 +4,14 @@ var now = new Date();
 var jsDate = now.getDate()+'-'+(now.getMonth()+1)+'-'+now.getFullYear();
 var messageB = 'Report Generated on ' + jsDate.toString() +  '- Copyright ARGO ' +now.getFullYear();
 
+function my_link(service){
+var baseUrl=urlI.split('?');
+ if (urlI.indexOf('services')!=-1) {
+window.location=baseUrl[0]+ '/' + service +'/endpoints?'+baseUrl[1];
+}
+else
+window.location=baseUrl[0]+ '/services?'+baseUrl[1];
+}
 
 if (~urlI.indexOf('egi')) {
     $('#ar').DataTable(
@@ -37,6 +45,67 @@ if (~urlI.indexOf('egi')) {
 }
 else {
     $('#ar').DataTable(
+        {
+            "dom": "Blftip",
+            "paging": true,
+            "autoWidth": true,
+            "lengthMenu": [[50, 25, 10, -1], [50, 25, 10, "All"]],
+            buttons: [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                {
+                    text: 'PDF',
+                    extend: 'pdfHtml5',
+                    footer: true,
+                    title: $("#titlePDF").val() + '\n' + $("#subtitlePDF").val() + '\n' ,
+                    filename: 'argo_ar_report.pdf',
+                    pageSize: 'A4',
+
+                    exportOptions: {
+                        columns: ':visible'
+                    }
+                }
+
+            ]
+
+        });
+
+
+}
+
+if (~urlI.indexOf('egi')) {
+    $('.ar').DataTable(
+        {
+            "dom": "Blftip",
+            "paging": true,
+            "autoWidth": true,
+            "lengthMenu": [[50, 25, 10, -1], [50, 25, 10, "All"]],
+            buttons: [
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                {
+                    text: 'PDF',
+                    action: function (e, dt, node, config) {
+
+
+                        if (~urlI.indexOf('?')) {
+                            var url = urlI.split('?');
+
+                            var Newurl = url[0] + '/pdf?' + url[1] + '&uri=' + uriL;
+                            window.location = Newurl;
+                        } else {
+                            window.location = urlI + '/pdf'+ '?uri=' + uriL;
+                        }
+                    }
+                }
+            ]
+
+        });
+}
+else {
+    $('.ar').DataTable(
         {
             "dom": "Blftip",
             "paging": true,
