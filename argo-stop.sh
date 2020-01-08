49dc253414fce3e757875352d54dd5858408d748
Cyril L'Orphelin <cyril.lorphelin@cc.in2p3.fr>
2020-01-08 18:09:29 +0100

    change script

--- argo-stop.sh
+++ argo-stop.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-cd /opt/argo-eosc
-HTTP_DISABLE=`sed ${0%/*}/../etc/service/service.properties -e '/^lavoisier.http.disable=/!d' -e 's/^lavoisier.http.disable=//'`
-if test "${HTTP_DISABLE}" = "true" ; then
-    PORT=`sed ${0%/*}/../etc/service/service.properties -e '/^lavoisier.https.port=/!d' -e 's/^lavoisier.https.port=//'`
-else
-    PORT=`sed ${0%/*}/../etc/service/service.properties -e '/^lavoisier.http.port=/!d' -e 's/^lavoisier.http.port=//'`
-fi
-curl http://localhost:${PORT}/lavoisier/restart
+
+pkill java
