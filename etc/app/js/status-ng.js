10bcdd70d89a9095e9fb433617fc0956cdd3cbad
Cyril L'Orphelin <cyril.lorphelin@cc.in2p3.fr>
2020-04-28 15:59:14 +0200

    MAJ UI 2

--- etc/app/js/status-ng.js
+++ etc/app/js/status-ng.js
@@ -12,7 +12,43 @@ $("#refreshPage").click(function() {
 
     });
 
+$(".toggle-body").click(function() {
+var id=$(this).attr('id');
+var pid=id.split('toggle_')[1];
+$("#body-"+pid).removeClass('d-none');
 
+$(this).addClass('d-none');
+});
+
+$(".untoggle-body").click(function() {
+var id=$(this).attr('id');
+var pid=id.split('untoggle_')[1];
+$("#body-"+pid).addClass('d-none');
+$("#toggle_"+pid).removeClass('d-none');
+});
+
+
+$(".filter-status").click(function() {
+
+var status=$(this).attr('id').split('status_')[1];
+var elementStatus='<input value="'+status+'"/>';
+filterStatus(elementStatus);
+$("#input-status-filter").val(status);
+
+});
+
+      function filterStatus(element) {
+            var status = $(element).val().toUpperCase();
+
+            $("span.status").each(function() {
+                if ($(this).text().search(status) > -1) {
+                    $(this).parents( ".card" ).show();
+                }
+                else {
+                    $(this).parents( ".card" ).hide();
+                }
+            });
+        }
 
 
 
@@ -28,19 +64,7 @@ $("#refreshPage").click(function() {
       function filter(element) {
             var value = $(element).val();
 
-            $("span.group").each(function() {
-                if ($(this).text().search(value) > -1) {
-                    $(this).parents( ".card" ).show();
-                }
-                else {
-                    $(this).parents( ".card" ).hide();
-                }
-            });
-        }
- function filter2(element) {
-            var value = $(element).val();
-
-            $("span.status").each(function() {
+            $("div.group").each(function() {
                 if ($(this).text().search(value) > -1) {
                     $(this).parents( ".card" ).show();
                 }
