11830fc7afae26014fc8a8152710270bcc09a470
Cyril L'Orphelin <cyril.lorphelin@cc.in2p3.fr>
2020-11-27 14:57:48 +0100

    MAJ Custom dashboard

--- etc/app/js/mydashboard.js
+++ etc/app/js/mydashboard.js
@@ -1,3 +1,49 @@
+
+    var start1 = moment().subtract(29, 'days');
+    var end1 = moment();
+
+    function cb(start, end) {
+        $('#reportrange1 span').html(start.format('YYYY-MM-DD') + ' / ' + end.format('YYYY-MM-DD'));
+    }
+
+    $('#reportrange1').daterangepicker({
+        startDate: start1,
+        endDate: end1,
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+           'Last 3 Months': [moment().subtract(3, 'month').startOf('month'), moment().endOf('month')],
+           'Last 6 Months': [moment().subtract(6, 'month').startOf('month'), moment().endOf('month')],
+        }
+    }, cb);
+
+    cb(start1, end1);
+
+
+        var start2 = moment().subtract(3, 'days');
+        var end2 = moment();
+
+        function cb2(start, end) {
+            $('#reportrange2 span').html(start.format('YYYY-MM-DD') + ' / ' + end.format('YYYY-MM-DD'));
+        }
+
+        $('#reportrange2').daterangepicker({
+            startDate: start2,
+            endDate: end2,
+            ranges: {
+               'Today': [moment(), moment()],
+               'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+               'Last 3 Days': [moment().subtract(3, 'days'), moment()]
+            }
+        }, cb);
+
+        cb2(start2, end2);
+
+
 $('#selectTenant').on('change', function() {
   var tenantId= this.value ;
   $("div.report").addClass("d-none");
@@ -8,12 +54,22 @@ $('#selectTenant').on('change', function() {
 
 $('.selectReport').on('change', function() {
   var reportId= this.value ;
-  console.log(reportId);
   $("div.group").addClass("d-none");
 
   $("#group_"+reportId).addClass("show");
    $("#group_"+reportId).removeClass("d-none");
 });
+
+$('#selectResults').on('change', function() {
+  var resultId= this.value ;
+
+  $("div.results").addClass("d-none");
+
+  $(".results_"+resultId).addClass("show");
+   $(".results_"+resultId).removeClass("d-none");
+   $("#submitF").removeClass("d-none");
+});
+
 
 $('span[data-toggle="tooltip"]').tooltip();
 $("#refreshPage").click(function() {
