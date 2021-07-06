ee49c6730e021dc3309a4d10b78ada4799078fe5
Cyril L'Orphelin <cyril.lorphelin@cc.in2p3.fr>
2021-07-06 11:23:41 +0200

    Add trends parameters like period and top selection

--- etc/app/js/trend.js
+++ etc/app/js/trend.js
@@ -4,11 +4,71 @@
         format: 'yyyy-mm-dd'
         });
 
-  $('#calendar').on('change', function () {
-           var urlI=window.location.href;
+         $('#calendar-range').click(function () {
+            $("input[value='range']").prop("checked", true);
+         });
+
+           $('#calendar').click(function () {
+                 $("input[value='date']").prop("checked", true);
+          });
+
+
+                    if ($("#start_date").val()!= undefined )
+                        var start = new Date($("#start_date").val());
+                    else
+                        var start = moment().subtract(3, 'days');
+
+                    if ($("#end_date").val()!= undefined )
+                        var end = new Date($("#end_date").val());
+                    else
+                        var end = moment();
+
+                    function cb(start, end) {
+                    if ($("#start_date").val()== undefined)
+                        $('#calendar-range').val(start.format('YYYY-MM-DD') + ' - ' + end.format('YYYY-MM-DD'));
+                    else
+                        $('#calendar-range').val(  start.toISOString().split('T')[0] + ' - ' + end.toISOString().split('T')[0]    );
+                    }
+
+
+                    $('#calendar-range').daterangepicker({
+                        startDate: start,
+                        endDate: end,
+                        locale: {
+                                    format: 'YYYY-MM-DD',
+                                },
+                       ranges: {
+                                         'Last 3 Days': [moment().subtract('days', 3), moment()],
+                                         'Last 10 Days': [moment().subtract('days', 10), moment()],
+                                         'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')],
+                                         'Last 3 Months': [moment().subtract('month', 3).startOf('month'), moment().subtract('month', 1).endOf('month')],
+                                         'Last 6 Months': [moment().subtract('month', 6).startOf('month'), moment().subtract('month', 1).endOf('month')]
+
+                           },
+                    }, cb);
+
+                    cb(start, end);
+
+
+
+ $("#parametersF").click(function () {
+
+        if ( $('input[name="period"]:checked').val()=='date' )
+        {
+        var urlI=window.location.href;
         var baseUrl=urlI.split('?');
-        window.location = baseUrl[0]+'?date='+$('#calendar').val();
+        window.location = baseUrl[0]+'?date='+$('#calendar').val()+'&top='+$("#top").val();
         $("#spinner").removeClass('d-none');
+        }
+        else
+        {
+
+          var range = 'start_date=' + $('#calendar-range').val().replace(' - ','&end_date=');
+          var urlI=window.location.href;
+          var baseUrl=urlI.split('?');
+           window.location = baseUrl[0]+'?'+range+'&top='+$("#top").val();
+          $("#spinner").removeClass('d-none');
+        }
 
 
   });
